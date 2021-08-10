40cc02714a5576ddb910edf28b557dd9a0249689
emyrzabekov <dreamyrza@gmail.com>
2021-08-10 19:40:52 +0600

    Add tasks model

---
+++ apps/tasks/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Task(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=False)
+    body = models.TextField()
+    is_finished = models.BooleanField(default=False)
+    estimated_finish_time = models.DateTimeField()
--- toDoList/settings.py
+++ toDoList/settings.py
@@ -30,14 +30,6 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +39,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'apps.tasks',
 ]
 
 ROOT_URLCONF = 'toDoList.urls'
